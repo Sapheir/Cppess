2d706651ae7de84d5d7928552641da0baf2a433a
TurcaVasileIonut <turcavasile235711@gmail.com>
2022-09-03 13:08:43 +0300

    #1
    removed .idea and debug

--- cmake-build-debug/_deps/gtest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Cppess/cmake-build-debug/_deps/gtest-src
-# Build directory: D:/Cppess/cmake-build-debug/_deps/gtest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Cppess/test
-# Build directory: D:/Cppess/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("D:/Cppess/cmake-build-debug/test/unit_tests[1]_include.cmake")
-subdirs("../_deps/gtest-build")
