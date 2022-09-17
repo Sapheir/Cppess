db3a57ce87a292cdcfcdd4fdb8232ba3635b5767
TurcaVasileIonut <turcavasile235711@gmail.com>
2022-09-17 16:08:30 +0300

    King new possible positions

--- src/Model/King/King.cpp
+++ src/Model/King/King.cpp
@@ -5,6 +5,39 @@
 #include "King.h"
 
 std::vector<std::pair<int, int>> King::nextPositions(int tableSize) const {
-    return nullptr;
-}
+    std::vector < std::pair < int, int >> allPositions;
 
+    /* we can decrease on x */
+    if(position_x > 1){
+        /* decrease on y */
+        if(position_y > 1)
+            allPositions.emplace_back(position_x - 1, position_y - 1);
+        /* same position in y */
+        allPositions.emplace_back(position_x - 1, position_y);
+        /* increase on y */
+        if(position_y < tableSize)
+            allPositions.emplace_back(position_x - 1, position_y + 1);
+    }
+
+    /* we can increase on x */
+    if(position_x > 1){
+        /* decrease on y */
+        if(position_y > 1)
+            allPositions.emplace_back(position_x + 1, position_y - 1);
+        /* same position in y */
+        allPositions.emplace_back(position_x + 1, position_y);
+        /* increase on y */
+        if(position_y < tableSize)
+            allPositions.emplace_back(position_x + 1, position_y + 1);
+    }
+
+    /* decrease only y */
+    if(position_y > 1)
+        allPositions.emplace_back(position_x, position_y - 1);
+    /* increase only y */
+    if(position_y < tableSize)
+        allPositions.emplace_back(position_x, position_y + 1);
+
+    return allPositions;
+
+}
