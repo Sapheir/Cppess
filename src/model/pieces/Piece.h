734acc20d9303e86205324a0dc342d3b2db70086
TurcaVasileIonut <turcavasile235711@gmail.com>
2022-09-27 18:46:16 +0300

    Added documentation for nextPosition in Piece.h

--- src/model/pieces/Piece.h
+++ src/model/pieces/Piece.h
@@ -11,6 +11,13 @@ class Piece{
 private:
     
 public:
+    /*
+     * Return a list of all possible positions inside the chess table where the bishop could be moved
+     *
+     * @param tableSize: the size of the table
+     *
+     * @return vector < pair < int, int > > - all possible positions
+     */
     [[nodiscard]] virtual std::vector < std::pair < int, int > > nextPositions(const int &tableSize) const = 0;
 
 };
